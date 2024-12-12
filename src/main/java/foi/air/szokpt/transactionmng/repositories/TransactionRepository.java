43aaddf5bbf77eaf94f030cd63c9fcfcc6879ddc
abernobic21 <126506127+abernobic21@users.noreply.github.com>
2024-12-12 13:03:59 +0100

    MI5-56 Dodana metoda za dohvat podataka detlja transakcije
    Dodana logika dohvaćanja podataka iz baze.

--- src/main/java/foi/air/szokpt/transactionmng/repositories/TransactionRepository.java
+++ src/main/java/foi/air/szokpt/transactionmng/repositories/TransactionRepository.java
@@ -7,9 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.lang.NonNull;
 
+import java.util.Optional;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Integer>,
         JpaSpecificationExecutor<Transaction> {
     @NonNull
     Page<Transaction> findAll(@NonNull Pageable pageable);
 
+    Optional<Transaction> findById(int id);
 }
