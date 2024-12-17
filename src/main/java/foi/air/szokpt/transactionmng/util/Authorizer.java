ea33e3dc22ebd74550a5e63255adba245c49c31d
abernobic21 <126506127+abernobic21@users.noreply.github.com>
2024-12-17 11:05:10 +0100

    MI5-84 Dodana mock klasa za provjeru jwt tokena
    Nakon implementacije provjere jwt tokena na servisu za upravljanjem računima, slati će zahtjev prema njemu.

---
+++ src/main/java/foi/air/szokpt/transactionmng/util/Authorizer.java
@@ -0,0 +1,10 @@
+package foi.air.szokpt.transactionmng.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Authorizer {
+    public void verifyToken(String authenticationHeader){
+
+    }
+}
