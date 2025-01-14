89aa50e6d51d3b1fd2a39f5359fb925acd008a42
abernobic21 <126506127+abernobic21@users.noreply.github.com>
2025-01-14 10:57:21 +0100

    Implementirana logika za slanje zahtjeva provjere jwt tokena
    Zaglavlje zaprimljenog zahtjeva se proslijeđuje u TokenValidationClient koji ga zatim šalje.

--- src/main/java/foi/air/szokpt/transactionmng/util/Authorizer.java
+++ src/main/java/foi/air/szokpt/transactionmng/util/Authorizer.java
@@ -1,10 +1,21 @@
 package foi.air.szokpt.transactionmng.util;
 
+import foi.air.szokpt.transactionmng.clients.TokenValidationClient;
+import foi.air.szokpt.transactionmng.exceptions.TokenValidationException;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Authorizer {
+    private final TokenValidationClient tokenValidationClient;
+
+    public Authorizer(TokenValidationClient tokenValidationClient){
+        this.tokenValidationClient = tokenValidationClient;
+    }
+
     public void verifyToken(String authenticationHeader){
+        if(tokenValidationClient.validateToken(authenticationHeader) == null){
+            throw new TokenValidationException();
+        }
 
     }
 }
